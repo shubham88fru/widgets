3cb5a1264e16cae17fb1a9759177c937cc58bcad
shubham88fru <shubham88fru@yahoo.com>
2021-04-07 09:59:23 +0530

    Initial commit for the widgets app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Accordian from './components/Accordian';
 
-function App() {
+const items = [
+  {
+    title: 'what is React?',
+    content: 'React is a front end javascript framework',
+  },
+  {
+    title: 'why use React?',
+    content: 'React is a favorite JS lib among engineers',
+  },
+  {
+    title: 'How to use React?',
+    content: 'You use React by creating components',
+  },
+];
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accordian items={items} />
     </div>
   );
-}
+};
 
 export default App;
