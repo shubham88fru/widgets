3d1a2a6711e3294821bb1967655a1455a2be5c83
shubham88fru <shubham88fru@yahoo.com>
2021-04-10 10:50:19 +0530

    finishing up accordion component

--- src/components/Accordian.js
+++ src/components/Accordian.js
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Accordian = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
   const onTitleClick = (idx) => {
-    console.log('Title clicked', idx);
+    setActiveIndex(idx);
   };
 
   const renderedItems = items.map((item, idx) => {
+    const active = idx === activeIndex ? 'active' : '';
     return (
       <React.Fragment key={item.title}>
-        <div className="title active" onClick={() => onTitleClick(idx)}>
+        <div
+          className={`title active ${active}`}
+          onClick={() => onTitleClick(idx)}
+        >
           <i className="dropdown icon"></i>
           {item.title}
         </div>
-        <div className="content active">
+        <div className={`content ${active}`}>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
     );
   });
+
   return <div className="ui styled accordion">{renderedItems}</div>;
 };
 
