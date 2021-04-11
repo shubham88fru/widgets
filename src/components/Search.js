50705c383865a1d478124f19962490f23cd01cad
shubham88fru <shubham88fru@yahoo.com>
2021-04-11 18:01:56 +0530

    implemenet search and display list of results

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,39 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
   const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
+
+  console.log(results);
 
   useEffect(() => {
-    console.log('Hi');
-  }); // null -> after every render, [] -> only after 1st render, [term] -> 1st render and whenever term changes
+    const search = async () => {
+      const { data } = await axios.get(`https://en.wikipedia.org/w/api.php`, {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+
+      setResults(data.query.search);
+    };
+    term && search();
+  }, [term]); // null -> after every render, [] -> only after 1st render, [term] -> 1st render and whenever term changes
+
+  const renderedResults = results.map((res) => {
+    return (
+      <div className="item" key={res.pageid}>
+        <div className="content">
+          <div className="header">{res.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: res.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
@@ -20,6 +48,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
