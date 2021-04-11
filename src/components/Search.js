afb98310d9089345d82a8d7becd7397bba55c11c
shubham88fru <shubham88fru@yahoo.com>
2021-04-11 12:42:00 +0530

    use effect hook

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Search = () => {
-  return <h1>SEARCH</h1>;
+  const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    console.log('Hi');
+  }); // null -> after every render, [] -> only after 1st render, [term] -> 1st render and whenever term changes
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label htmlFor="ipt">Enter Search Term</label>
+          <input
+            id="ipt"
+            className="input"
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
